c6b1a37032c2be9dc97f90238006f5c2e9164940
Erik <erikcui9020@gmail.com>
2025-06-25 13:32:39 -0700

    refactor store out to a separate .ts file

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,31 +14,8 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
-// @ts-ignore TODO: IDE inline complains ESM but this is working: TS1479
-import Store from 'electron-store';
-import chromePaths from 'chrome-paths';
+import store from './store';
 import puppeteer from 'puppeteer-core';
-
-// instantiate store schema and store
-type StoreType = {
-  testBool?: boolean,
-  defaultChromePath: string,
-  userChromePath?: string,
-};
-
-const store = new Store<StoreType>({
-  defaults: {
-    defaultChromePath: chromePaths.chrome,
-  }
-});
-
-// IPC listener for electron-store
-ipcMain.on('electron-store-get', async (event, val) => {
-  event.returnValue = store.get(val);
-});
-ipcMain.on('electron-store-set', async (event, key, val) => {
-  store.set(key, val);
-});
 
 ipcMain.on('puppet-get', async (event, val) => {
   const browser = await puppeteer.launch({
