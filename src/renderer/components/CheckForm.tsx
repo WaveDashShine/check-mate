6cafc9419fb54f8d02f23f4ce64d4b48290f3bc5
Erik <erikcui9020@gmail.com>
2025-07-23 13:38:54 -0700

    add stub form structure

--- src/renderer/components/CheckForm.tsx
+++ src/renderer/components/CheckForm.tsx
@@ -1,12 +1,34 @@
 import Form, { BaseFormProps } from 'src/renderer/components/Form';
+import 'src/renderer/components/CheckForm.css';
 
 function CheckFormFields() {
-  // TODO: finish defining the rest of the fields
-  // TODO: how to pass these form values to the handleConfirm method??
   return (
     <div>
       <label>Name</label>
-      <input />
+      <input type="text" />
+      <label>Note</label>
+      <textarea />
+      <label>Enabled</label>
+      <button role="switch" aria-checked="true"></button>
+      <label>Frequency (seconds)</label>
+      <input role="spinbutton" step="1" type="number" />
+      <div>
+        <label>Browser Config</label>
+        <label>Link</label>
+        <input type="text" />
+        <label>Check Text</label>
+        <button role="switch" aria-checked="false"></button>
+        <label>Check HTML</label>
+        <button role="switch" aria-checked="false"></button>
+        <label>Check Screenshot</label>
+        <button role="switch" aria-checked="false"></button>
+        <label>Locator</label>
+        <input type="text" />
+      </div>
+      <label>Alert History</label>
+      <table></table>
+      <label>Tags</label>
+      <table></table>
     </div>
   );
 }
