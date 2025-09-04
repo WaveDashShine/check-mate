f20bfcc345c3b1b0ed12f9a632e045b2c007d31f
Erik <erikcui9020@gmail.com>
2025-09-04 13:34:05 -0700

    update browser code for taking screenshots + html/text parsing

--- src/main/browser.ts
+++ src/main/browser.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Page, Browser } from 'puppeteer-core';
+import puppeteer, { Browser, Page, ScreenshotOptions } from 'puppeteer-core';
 import store from 'src/main/main';
 import StoreKeys from 'src/storeConfig';
 import path from 'path';
@@ -16,9 +16,13 @@ export async function launchBrowser(isHeadless: boolean = false) {
   });
 }
 
+function isValidLocator(locator: string) {
+  return !(locator === '' || locator == undefined);
+}
+
 async function checkText(page: Page, locator: string): Promise<string> {
-  if (locator === '') {
-    locator = 'div';
+  if (isValidLocator(locator)) {
+    locator = 'body';
   }
   await page.locator(locator).wait();
   const elementText = await page.$eval(locator, (el) => el.textContent);
@@ -26,23 +30,77 @@ async function checkText(page: Page, locator: string): Promise<string> {
 }
 
 async function checkHtml(page: Page, locator: string): Promise<string> {
-  return ''; // stub
+  if (isValidLocator(locator)) {
+    return await page.$eval(locator, (el) => el.innerHTML);
+  }
+  return await page.content();
+}
+
+async function checkScreenshot(
+  page: Page,
+  locator: string,
+): Promise<Uint8Array> {
+  const options: ScreenshotOptions = { fullPage: true, encoding: 'base64' };
+  if (isValidLocator(locator)) {
+    const boundingBox: DOMRect = await page
+      .$eval(locator, (el) => el.getBoundingClientRect())
+      .then((result) => {
+        console.log('screenshot bounding box', result);
+        return result;
+      });
+    if (
+      boundingBox.x &&
+      boundingBox.y &&
+      boundingBox.height &&
+      boundingBox.width
+    ) {
+      // defaults to full page if bounding box fails
+      options.fullPage = false;
+      options.clip = {
+        x: boundingBox.x,
+        y: boundingBox.y,
+        height: boundingBox.height,
+        width: boundingBox.width,
+      };
+    }
+  }
+  return await page
+    .screenshot(options)
+    .then((result) => {
+      console.log('screenshot successful');
+      return result;
+    })
+    .catch((error) => {
+      console.log('screenshot failed');
+      console.log(error);
+      return error;
+    });
 }
 
 export async function BrowserCheck(checkConfig: CheckDb): Promise<Discovery> {
-  const browser: Browser = await launchBrowser(false); // TODO: should be true
+  // TODO: should be headless or as config
+  const browser: Browser = await launchBrowser(false);
   const page: Page = await browser.newPage();
+  page.setDefaultTimeout(10000); // ms
   await page.goto(checkConfig.browserConfig.url);
-  const locator = checkConfig.browserConfig.locator;
-  if (locator !== '' || locator !== undefined) {
-    if (checkConfig.browserConfig.checkText) {
-      const locatorText = await checkText(page, locator);
-    }
-  }
-  // TODO: return discovery object?
-  // TODO: discovery id will need to be handled in the frontend
-  await browser.close();
   let discovery: Discovery = {} as Discovery;
+  const locator = checkConfig.browserConfig.locator;
+  if (checkConfig.browserConfig.checkText) {
+    discovery.text = await checkText(page, locator).then((result) => {
+      console.log('Text successful', result);
+      return result;
+    });
+  }
+  if (checkConfig.browserConfig.checkHtml) {
+    discovery.html = await checkHtml(page, locator).then((result) => {
+      console.log('HTML successful', result);
+      return result;
+    });
+  }
+  if (checkConfig.browserConfig.checkScreenshot) {
+    discovery.screenshot = await checkScreenshot(page, locator);
+  }
+  await browser.close();
   discovery.timestamp = new Date();
   return discovery;
 }
