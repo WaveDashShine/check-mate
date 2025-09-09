d18e3aca8a6f4e9458d7c9208878292df402615d
Erik <erikcui9020@gmail.com>
2025-09-09 12:50:39 -0700

    re-work drawer re-sizing with a resize element on the side of the drawer + fix overlapping drawer resize issue

--- src/renderer/components/generic/Drawer.tsx
+++ src/renderer/components/generic/Drawer.tsx
@@ -1,6 +1,6 @@
 import 'src/renderer/components/generic/Drawer.css';
 import { ReactNode } from 'react';
-import useResize from 'src/renderer/hooks/useResize';
+import { useCallback, useEffect, useState } from 'react';
 
 interface DrawerProps {
   isOpen: boolean;
@@ -9,9 +9,37 @@ interface DrawerProps {
 }
 
 function Drawer(props: DrawerProps) {
-  const { width, enableResize } = useResize({ minWidth: 250 });
+  const minWidth = 250;
+  const [isResizing, setIsResizing] = useState(false);
+  const [width, setWidth] = useState(minWidth);
+
+  const resize = useCallback(
+    (e: MouseEvent) => {
+      if (isResizing) {
+        e.preventDefault();
+        const newWidth = window.innerWidth - e.clientX;
+        if (newWidth >= minWidth && newWidth <= window.innerWidth) {
+          setWidth(newWidth);
+        }
+      }
+    },
+    [minWidth, isResizing, setWidth],
+  );
+  useEffect(() => {
+    if (isResizing) {
+      document.addEventListener('mousemove', resize);
+      return () => {
+        document.removeEventListener('mousemove', resize);
+      };
+    }
+  }, [isResizing, resize]);
+
   return (
-    <>
+    <div
+      onMouseUp={() => {
+        setIsResizing(false);
+      }}
+    >
       {/* Backdrop */}
       {props.isOpen && (
         <div className="drawer-backdrop" onDoubleClick={props.onClose} />
@@ -20,15 +48,27 @@ function Drawer(props: DrawerProps) {
       {/* Drawer panel */}
       <div
         className={`drawer ${props.isOpen ? 'open' : ''}`}
-        onMouseDown={enableResize}
         style={props.isOpen ? { width } : {}}
       >
+        <span
+          className={'drawer-resizer'}
+          onMouseOver={() => {
+            document.body.style.cursor = 'col-resize';
+          }}
+          onMouseLeave={() => {
+            document.body.style.cursor = 'default';
+          }}
+          onMouseDown={(e) => {
+            setIsResizing(true);
+            e.preventDefault();
+          }}
+        ></span>
         <button className="drawer-close" onClick={props.onClose}>
           ×
         </button>
         <div className="drawer-content">{props.children}</div>
       </div>
-    </>
+    </div>
   );
 }
 
