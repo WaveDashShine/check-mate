d3c91df2338eb370fc116a9e9c87c08f237ecc85
Erik <erikcui9020@gmail.com>
2025-09-08 18:24:09 -0700

    fix screenshot display code

--- src/renderer/components/fields/Screenshot.tsx
+++ src/renderer/components/fields/Screenshot.tsx
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from 'react';
-
 interface ScreenshotProps {
   data: Uint8Array | undefined; // PNG bytes
 }
 
 function Screenshot(props: ScreenshotProps) {
-  const [url, setUrl] = useState<string>();
+  if (!props.data) return <p>No image data</p>;
 
-  useEffect(() => {
-    if (props.data) {
-      const blob = new Blob([props.data], { type: 'image/png' });
-      const objectUrl = URL.createObjectURL(blob);
-      setUrl(objectUrl);
-
-      return () => {
-        URL.revokeObjectURL(objectUrl); // cleanup when component unmounts or data changes
-      };
-    }
-  }, [props.data]);
-
-  if (!url) return <p>No image data</p>;
-
-  return <img src={url} alt="screenshot" style={{ maxWidth: '100%' }} />;
+  return (
+    <img
+      src={`data:image/png;base64,${props.data}`}
+      alt="screenshot"
+      style={{ maxWidth: '100%' }}
+    />
+  );
 }
 
 export default Screenshot;
